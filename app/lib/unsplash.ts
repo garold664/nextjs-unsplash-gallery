0d8102836ec140ef98d657848b0528923ff2d749
garold664 <garold664@gmail.com>
2024-05-28 15:45:05 +0300

    added images from unsplash api

---
+++ app/lib/unsplash.ts
@@ -0,0 +1,19 @@
+import { Photo } from './types';
+
+const headers = {
+  Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+};
+
+export const getPhotos = async () => {
+  const url = new URL('https://api.unsplash.com/photos/');
+
+  url.searchParams.set('per_page', '10');
+  url.searchParams.set('order_by', 'popular');
+
+  return (await fetch(url, { headers }).then((res) => res.json())) as Photo[];
+};
+
+export const getPhoto = async (id: string) => {
+  const url = new URL(`https://api.unsplash.com/photos/${id}`);
+  return await fetch(url, { headers }).then((res) => res.json());
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 import Image from 'next/image';
 import Box from './components/Box';
+import { getPhotos } from './lib/unsplash';
+import Link from 'next/link';
 
-export default function Home() {
-  return <Box className="bg-slate-500">Home Page</Box>;
+export default async function Home() {
+  const photos = await getPhotos();
+  // console.log(photos);
+  return (
+    <div>
+      {photos.map((photo) => (
+        <Link href={`/photos/${photo.id}`} key={photo.id}>
+          <Image
+            src={photo.urls.regular}
+            alt={photo.alt_description}
+            width={400}
+            height={400}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
